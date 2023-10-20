04def21e03eb76a57f3d52a02247d49ab79eeb95
Camila Paladines <camilapaladines27@gmail.com>
2023-10-20 09:36:50 -0500

    Add base code for challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	const [products, setProducts] = useState<Product[]>([]);
+	const [search, setSearch] = useState<string>('');
+
+	// API: https://fakestoreapi.com/products
+
+	return (
+		<div className='App'>
+			<input
+				className='search-bar'
+				type='text'
+				placeholder='Search'
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<ul className='products-list'></ul>
+		</div>
+	);
 }
 
 export default App;
